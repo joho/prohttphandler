eb56e50a47e55f0c35e58fbc5a8a07a50b6c7b49
John Barton (joho) <jrbarton@gmail.com>
2013-09-18 17:06:39 +1000

    go doc up the library

--- prohttphandler.go
+++ prohttphandler.go
@@ -1,3 +1,5 @@
+// A totally pro http handler. One that isn't particularly pro at all,
+// it was just handy and I needed it in multiple projects.
 package prohttphandler
 
 import (
@@ -5,11 +7,16 @@ import (
 	"strings"
 )
 
+// The actuall http handler
 type ProHttpHandler struct {
 	fsHandler             http.Handler
 	exactMatchHandleFuncs map[string]func(http.ResponseWriter, *http.Request)
 }
 
+// ServeHTTP will do one of three things:
+//   * It will check if there is a handler func registered for the *exact* path of the request
+//   * If there is no exact match, it will try and find a file in the static asset path to serve (no dir listings though)
+//   * If neither of the above, it will 404 in an ugly way
 func (h *ProHttpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	handleFunc, handleFuncPresent := h.exactMatchHandleFuncs[r.URL.Path]
 	if handleFuncPresent == true {
@@ -21,10 +28,13 @@ func (h *ProHttpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Registers a http handler func for the path you specify
 func (h *ProHttpHandler) ExactMatchFunc(path string, handlerFunc func(http.ResponseWriter, *http.Request)) {
 	h.exactMatchHandleFuncs[path] = handlerFunc
 }
 
+// Create a ProHttpHandler
+// You need to give it a path to use as the static assets root
 func New(publicStaticAssetPath string) (h *ProHttpHandler) {
 	h = new(ProHttpHandler)
 	h.exactMatchHandleFuncs = map[string]func(http.ResponseWriter, *http.Request){}
